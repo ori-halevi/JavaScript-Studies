048c88050d50e202ee5577f6e0c164e6eeea59c8
ori halevi <orihalevi98@gmail.com>
2024-06-05 09:45:05 +0300

    2

--- 03.06.24 - שיעור 9/תרגיל 2/1.js
+++ 03.06.24 - שיעור 9/תרגיל 2/1.js
@@ -3,36 +3,43 @@ const dayOfTheWeekDiv = document.getElementsByClassName("day-of-the-week-div");
 const hoursDiv = document.getElementsByClassName("hours-div");
 const minutesDiv = document.getElementsByClassName("minutes-div");
 const secondsDiv = document.getElementsByClassName("seconds-div");
+
 const amPmDiv = document.getElementsByClassName("am-pm-div");
 
-let hours = new Date().getHours();
-let twelveHourClock = "am";
-if (hours > 12) {
-  hours = hours - 12;
-  twelveHourClock = "pm";
+setInterval(() => {
+  showTime();
+}, 1000);
+
+function showTime() {
+  let hours = new Date().getHours();
+  let twelveHourClock = "am";
+  if (hours > 12) {
+    hours = hours - 12;
+    twelveHourClock = "pm";
+  }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  let dayOfTheWeek = new Date().getDay();
+
+  let minutes = new Date().getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  let seconds = new Date().getSeconds();
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  dayOfTheWeekDiv[0].textContent = idxToStrDay(dayOfTheWeek);
+
+  hoursDiv[0].textContent = hours;
+  minutesDiv[0].textContent = minutes;
+  secondsDiv[0].textContent = seconds;
+  amPmDiv[0].textContent = twelveHourClock;
 }
-if (hours < 10) {
-  hours = "0" + hours;
-}
-
-let dayOfTheWeek = new Date().getDay();
-
-let minutes = new Date().getMinutes();
-if (minutes < 10) {
-  minutes = "0" + minutes;
-}
-
-let seconds = new Date().getSeconds();
-if (seconds < 10) {
-  seconds = "0" + minutes;
-}
-
-dayOfTheWeekDiv[0].textContent = idxToStrDay(dayOfTheWeek);
-
-hoursDiv[0].textContent = hours;
-minutesDiv[0].textContent = minutes;
-secondsDiv[0].textContent = seconds;
-amPmDiv[0].textContent = twelveHourClock;
 
 function idxToStrDay(dayIdx) {
   if (dayIdx === 0) return "Sunday";
