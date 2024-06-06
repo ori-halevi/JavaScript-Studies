d72f06d067d5d42874066ba33cb7aa75f27204b2
ori halevi <orihalevi98@gmail.com>
2024-06-06 07:53:59 +0300

    אתר מתכונים

---
+++ אתר מתכונים/recipes code.js
@@ -0,0 +1,15 @@
+
+
+
+document.getElementById("submitButton").onclick = function () {
+
+    userEmail = document.getElementById("emailInput").value;
+    userName = document.getElementById("nameInput").value;
+    userMessage = document.getElementById("messageInput").value;
+
+    
+    console.log(userEmail);
+    console.log(userName);
+    console.log(userMessage);
+}
+
