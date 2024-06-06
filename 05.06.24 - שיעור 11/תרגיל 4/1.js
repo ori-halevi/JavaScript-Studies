e29d40c15f8d48089a00fcac05825372c307b684
ori halevi <orihalevi98@gmail.com>
2024-06-06 07:36:49 +0300

    Update 1.js

--- 05.06.24 - שיעור 11/תרגיל 4/1.js
+++ 05.06.24 - שיעור 11/תרגיל 4/1.js
@@ -1,46 +1,43 @@
-function getInfo(name) {
-  return new Promise((resolve, reject) => {
-    fetch(`https://jsonplaceholder.typicode.com/users/?username=${name}`)
-      .then((res) => {
-        if (res.ok) {
-          return res.json(); // Parse the JSON from the response body
-        } else {
-          throw new Error("Network response was not ok.");
-        }
-      })
-      .then((data) => {
-        if (data.length !== 0) {
-          console.log(data);
-          resolve("good");
-        } else {
-          throw new Error("The value is empty!");
-        }
-      })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-        reject("bad");
-      });
-  });
-}
+// function getInfo(name) {
+//   return new Promise((resolve, reject) => {
+//     fetch(`https://jsonplaceholder.typicode.com/users/?username=${name}`)
+//       .then((res) => {
+//         if (res.ok) {
+//           return res.json(); // Parse the JSON from the response body
+//         } else {
+//           throw new Error("Network response was not ok.");
+//         }
+//       })
+//       .then((data) => {
+//         if (data.length !== 0) {
+//           console.log(data);
+//           resolve("good");
+//         } else {
+//           throw new Error("The value is empty!");
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("There was a problem with the fetch operation:", error);
+//         reject("bad");
+//       });
+//   });
+// }
 
 async function readFile(name) {
   try {
-    const getInfoResult = await getInfo(name);
-    console.log(getInfoResult);
+    const getInfoResult = await fetch(
+      `https://jsonplaceholder.typicode.com/users/?username=${name}`
+    );
+    const asJSON = await getInfoResult.json();
+    if (asJSON.length !== 0) {
+      console.log(asJSON);
+    } else {
+      throw new Error("The value is empty!");
+    }
+
   } catch (error) {
     console.error("Error reading file:", error);
   }
 }
 
 readFile("Bret");
-
-function rwt(resolve, reject) {
-  if (0 === 0) {
-
-    resolve("yes");
-  } else {
-    reject("no");
-  }
-}
-console.log(rwt());
-
